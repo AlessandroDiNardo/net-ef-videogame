5c0bd5b19c03a52d5af932357580b7a065746016
AlessandroDiNardo <alexdna003@gmail.com>
2023-04-21 17:50:17 +0200

    create migrations and the first two methods

--- net-ef-videogame/SoftwareHouse.cs
+++ net-ef-videogame/SoftwareHouse.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace net_ef_videogame
 {
-    internal class SoftwareHouse
+    [Table("softwarehouses")]
+    public class SoftwareHouse
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        [MaxLength(32)]
+        public string? Name { get; set; }
+
+        [Column("city")]
+        [Required]
+        [MaxLength (64)]
+        public string? City { get; set; }  
+        
+        public List<Videogame>? Videogames { get; set; }
+
     }
 }
--- net-ef-videogame/Videogame.cs
+++ net-ef-videogame/Videogame.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace net_ef_videogame
 {
-    internal class Videogame
+    [Table("videogames")]
+    public class Videogame
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        [MaxLength(64)]
+        public string? Name { get; set; }
+
+        [Column("overview")]
+        [Required]
+        [MaxLength(500)]
+        public string? Overview { get; set; }
+        [Column("release_date")]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [ForeignKey("SoftwareHouse")] 
+        [Column("software_house_id")]
+        [Required]
+
+        public int SoftwareHouseId { get; set; }
+        public SoftwareHouse? SoftwareHouse { get; set; }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Overview: {Overview}");
+            Console.WriteLine($"Release Date: {ReleaseDate:dd/MM/yyyy}");
+            Console.WriteLine($"Software House: {SoftwareHouse?.Name ?? "Unknown"}");
+        }
     }
 }
